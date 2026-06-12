34f52d9f87d0fa8a9de98b78f2ae33da87d1adb3
drogue-bit <swapnil.pal112@gmail.com>
2026-06-12 12:22:48 +0530

    unnecessary files removed

--- exp2.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://open.spotify.com/user/31junzitapcjrhhvlvg2ofuwwsey/recently-played-artists"
-
-html = requests.get(url).text
-
-print(len(html))
-print(html[:1000])
